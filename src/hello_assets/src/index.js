58f0b0894dbece0c8031815a8a86b96901e842b4
guorong <dudurong009@gmail.com>
2022-06-28 23:00:16 +0800

    Fix some issues

--- src/hello_assets/src/index.js
+++ src/hello_assets/src/index.js
@@ -2,7 +2,7 @@ import { hello } from "../../declarations/hello";
 
 document.querySelector("form").addEventListener("submit", async (e) => {
   e.preventDefault();
-  document.getElementById("greeting").innerText = "";
+
   const loader = document.getElementById("loader");
 
   const button = e.target.querySelector("button");
@@ -18,9 +18,11 @@ document.querySelector("form").addEventListener("submit", async (e) => {
     const publicKey = await window.ic.plug.requestConnect();
     console.log(`The connected user's public key is:`, publicKey);
 
-    document.getElementById("principalId").innerText = publicKey;
+    // document.getElementById("principalId").innerText = publicKey;
 
     const result = await window.ic.plug.isConnected();
+    document.getElementById("connectionStatus").innerText = `Connected: ${result}`;
+
     console.log(`Plug connection is ${result}`);
   } catch (e) {
     console.log(e);
